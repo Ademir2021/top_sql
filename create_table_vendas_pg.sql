c99b0238cbed267a3405d5fffb907bdead150ecd
Ademir <ademir@debian.local>
2023-04-05 22:17:20 -0300

    up

--- create_table_vendas_pg.sql
+++ create_table_vendas_pg.sql
@@ -1,125 +1,128 @@
-CREATE TABLE
-  public.filiais ( 
-    id_filial serial NOT NULL,
-    created_at timestamp without time zone NOT NULL DEFAULT now(),
-    name_filial character varying(60) NULL
-  );
-
-ALTER TABLE
-  public.filiais
-ADD
-  CONSTRAINT filiais_pkey PRIMARY KEY (id_filial)
-
+-- 01
 
 CREATE TABLE
-  public.brands ( 
-    id_brand serial NOT NULL,
-    created_at timestamp without time zone NOT NULL DEFAULT now(),
-    name_brand VARCHAR (60)
-  );
-
+public.filiais ( 
+id_filial serial NOT NULL,
+created_at timestamp without time zone NOT NULL DEFAULT now(),
+name_filial character varying(60) NULL
+);
 ALTER TABLE
-  public.brands
+public.filiais
 ADD
-  CONSTRAINT brands_pkey PRIMARY KEY (id_brand)
+CONSTRAINT filiais_pkey PRIMARY KEY (id_filial)
 
+-- 02
 
 CREATE TABLE
-  public.sectors ( 
-    id_sector serial NOT NULL,
-    created_at timestamp without time zone NOT NULL DEFAULT now(),
-    name_sector VARCHAR (60)
-  );
-
+public.brands ( 
+id_brand serial NOT NULL,
+created_at timestamp without time zone NOT NULL DEFAULT now(),
+name_brand VARCHAR (60)
+);
 ALTER TABLE
-  public.sectors
+public.brands
 ADD
-  CONSTRAINT sectors_pkey PRIMARY KEY (id_sector)
+CONSTRAINT brands_pkey PRIMARY KEY (id_brand)
+
+-- 03
 
 CREATE TABLE
-  public.persons ( 
-    id_person serial NOT NULL,
-    created_at timestamp without time zone NOT NULL DEFAULT now(),
-    name_pers character varying(60) NULL,
-    cpf_pers character varying(11) NULL,
-    andress_pers character varying(60) NULL,
-    fk_name_filial integer NOT NULL,
-    FOREIGN KEY (fk_name_filial) REFERENCES filiais(id_filial) ON DELETE CASCADE
- 
-  );
-
+public.sectors ( 
+id_sector serial NOT NULL,
+created_at timestamp without time zone NOT NULL DEFAULT now(),
+name_sector VARCHAR (60)
+);
 ALTER TABLE
-  public.persons
+public.sectors
 ADD
-  CONSTRAINT persons_pkey PRIMARY KEY (id_person)
-/**end 04*/
+CONSTRAINT sectors_pkey PRIMARY KEY (id_sector)
+
+-- 04
 
 CREATE TABLE
-  public.sales ( 
-    id_sale serial NOT NULL,
-    created_at timestamp without time zone NOT NULL DEFAULT now(),
-    fk_name_pers serial,
-    val_rec NUMERIC(18,2) NULL,
-    disc_sale NUMERIC(18,2) NULL,
-    total_sale NUMERIC(18,2) NULL,
-    fk_name_filial integer NOT NULL,
-    fk_name_user integer NOT NULL,
-    FOREIGN KEY (fk_name_pers) REFERENCES persons(id_person) ON DELETE CASCADE
-  );
-
+public.persons ( 
+id_person serial NOT NULL,
+created_at timestamp without time zone NOT NULL DEFAULT now(),
+name_pers character varying(60) NULL,
+cpf_pers character varying(11) NULL,
+address_pers character varying(60) NULL,
+fk_name_filial integer NOT NULL,
+FOREIGN KEY (fk_name_filial) REFERENCES filiais(id_filial) ON DELETE CASCADE
+);
 ALTER TABLE
-  public.sales
+public.persons
 ADD
-  CONSTRAINT sales_pkey PRIMARY KEY (id_sale)
+CONSTRAINT persons_pkey PRIMARY KEY (id_person)
+
+-- 05
 
 CREATE TABLE
-  public.products ( 
-    id_product serial NOT NULL,
-    created_at timestamp without time zone NOT NULL DEFAULT now(),
-    descric_product VARCHAR(60) NULL,
-    val_max_product NUMERIC(18,2) NULL,
-    val_min_product NUMERIC(18,2) NULL,
-    fk_brand integer,
-    fk_sector integer,
-    bar_code VARCHAR(200) NULL,
-    FOREIGN KEY (fk_brand) REFERENCES brands(id_brand) ON DELETE CASCADE,
-    FOREIGN KEY (fk_sector) REFERENCES sectors(id_sector) ON DELETE CASCADE
-  );
-
+public.sales ( 
+id_sale serial NOT NULL,
+created_at timestamp without time zone NOT NULL DEFAULT now(),
+fk_name_pers serial,
+val_rec NUMERIC(18,2) NULL,
+disc_sale NUMERIC(18,2) NULL,
+total_sale NUMERIC(18,2) NULL,
+fk_name_filial integer NOT NULL,
+fk_name_user integer NOT NULL,
+FOREIGN KEY (fk_name_pers) REFERENCES persons(id_person) ON DELETE CASCADE
+);
 ALTER TABLE
-  public.products
+public.sales
 ADD
-  CONSTRAINT products_pkey PRIMARY KEY (id_product)
+CONSTRAINT sales_pkey PRIMARY KEY (id_sale)
+
+-- 06
 
 CREATE TABLE
-  public.itens_sale ( 
-    id_item_sequen serial NOT NULL,
-    created_at timestamp without time zone NOT NULL DEFAULT now(),
-    fk_sale INTEGER,
-    fk_product INTEGER,
-    amount_product INTEGER,
-    val_product NUMERIC(18,2),
-    total_product NUMERIC(18,2),
-    FOREIGN KEY (fk_sale) REFERENCES sales(id_sale) ON DELETE CASCADE,
-    FOREIGN KEY (fk_product) REFERENCES products(id_product) ON DELETE CASCADE
-  );
-
+public.products ( 
+id_product serial NOT NULL,
+created_at timestamp without time zone NOT NULL DEFAULT now(),
+descric_product VARCHAR(60) NULL,
+val_max_product NUMERIC(18,2) NULL,
+val_min_product NUMERIC(18,2) NULL,
+fk_brand integer,
+fk_sector integer,
+bar_code VARCHAR(200) NULL,
+FOREIGN KEY (fk_brand) REFERENCES brands(id_brand) ON DELETE CASCADE,
+FOREIGN KEY (fk_sector) REFERENCES sectors(id_sector) ON DELETE CASCADE
+);
 ALTER TABLE
-  public.itens_sale
+public.products
 ADD
-  CONSTRAINT itens_sale_pkey PRIMARY KEY (id_item_sequen, fk_sale)
-  /**end 07*/
+CONSTRAINT products_pkey PRIMARY KEY (id_product)
 
-  CREATE TABLE
-  public.users (
-    id serial NOT NULL,
-    created_at timestamp without time zone NOT NULL DEFAULT now(),
-    name character varying(60) NULL,
-    username character varying(60) NULL,
-    password character varying(255) NULL
-  );
+-- 07
 
+CREATE TABLE
+public.itens_sale ( 
+id_item_sequen serial NOT NULL,
+created_at timestamp without time zone NOT NULL DEFAULT now(),
+fk_sale INTEGER,
+fk_product INTEGER,
+amount_product INTEGER,
+val_product NUMERIC(18,2),
+total_product NUMERIC(18,2),
+-- FOREIGN KEY (fk_sale) REFERENCES sales(id_sale) ON DELETE CASCADE,
+FOREIGN KEY (fk_product) REFERENCES products(id_product) ON DELETE CASCADE
+);
 ALTER TABLE
-  public.users
+public.itens_sale
 ADD
-  CONSTRAINT users_pkey PRIMARY KEY (id)
+CONSTRAINT itens_sale_pkey PRIMARY KEY (id_item_sequen, fk_sale)
+
+-- 08
+
+CREATE TABLE
+public.users (
+id serial NOT NULL,
+created_at timestamp without time zone NOT NULL DEFAULT now(),
+name character varying(60) NULL,
+username character varying(60) NULL,
+password character varying(255) NULL
+);
+ALTER TABLE
+public.users
+ADD
+CONSTRAINT users_pkey PRIMARY KEY (id)
