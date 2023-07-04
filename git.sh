021b2a6c9d674b1cc6ce7a0f051e5a1c56a61536
Ademir <ademir@debian.local>
2023-07-04 10:48:25 -0300

    stns

--- git.sh
+++ git.sh
@@ -1,10 +1,8 @@
 #bin/bash
 clear
-# cd ../../Documentos/git/
-# ./git.sh
+Documentos/git/./git.sh
 echo "<< init >>"
 git add .
 git commit -m "stns"
 git push -u origin master
 echo "<< end >>"
-# clear
