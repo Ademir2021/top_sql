ac5819137fd04b99c6c219c70f1cb779d9e8a880
Ademir Souza de Almeida <centroserra@gmail.com>
2024-05-23 13:54:08 -0300

    setings - ?

--- reset_sales.sql
+++ reset_sales.sql
@@ -11,3 +11,18 @@ ALTER SEQUENCE sales_id_sale_seq RESTART WITH 1
 
 SELECT *FROM itens_sale_id_item_sequen_seq
 ALTER SEQUENCE itens_sale_id_item_sequen_seq RESTART WITH 1
+
+-- RESET FULL --
+DELETE FROM sales
+-- SELECT *FROM sales_id_sale_seq
+ALTER SEQUENCE sales_id_sale_seq RESTART WITH 1
+-- DELETE FROM itens_sale
+ALTER SEQUENCE itens_sale_id_item_sequen_seq RESTART WITH 1
+DELETE FROM refresh_token
+ALTER SEQUENCE refresh_token_id_seq RESTART WITH 1
+DELETE FROM users
+ALTER SEQUENCE users_id_seq RESTART WITH 1
+DELETE FROM persons
+ALTER SEQUENCE persons_id_person_seq RESTART WITH 1
+DELETE FROM contacts
+ALTER SEQUENCE contacts_id_seq RESTART WITH 1
