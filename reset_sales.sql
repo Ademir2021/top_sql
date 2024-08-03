da0ad3b01d8cdf8677b5685366b3c420d6fa1070
Ademir2021 <centroserra@gmail.com>
2024-08-03 16:51:38 -0300

    setings - ?

--- reset_sales.sql
+++ reset_sales.sql
@@ -13,20 +13,23 @@ SELECT *FROM itens_sale_id_item_sequen_seq
 ALTER SEQUENCE itens_sale_id_item_sequen_seq RESTART WITH 1
 
 -- RESET FULL --
-DELETE FROM vals_recebidos
-ALTER SEQUENCE vals_recebidos_id_val_seq RESTART WITH 1
-DELETE FROM contas_receber
-ALTER SEQUENCE contas_receber_id_conta_seq RESTART WITH 1
-DELETE FROM sales
--- SELECT *FROM sales_id_sale_seq
-ALTER SEQUENCE sales_id_sale_seq RESTART WITH 1
--- DELETE FROM itens_sale
-ALTER SEQUENCE itens_sale_id_item_sequen_seq RESTART WITH 1
-DELETE FROM refresh_token
-ALTER SEQUENCE refresh_token_id_seq RESTART WITH 1
-DELETE FROM users
-ALTER SEQUENCE users_id_seq RESTART WITH 1
-DELETE FROM persons
-ALTER SEQUENCE persons_id_person_seq RESTART WITH 1
-DELETE FROM contacts
-ALTER SEQUENCE contacts_id_seq RESTART WITH 1
+DELETE FROM caixa_mov;
+ALTER SEQUENCE caixa_mov_id_caixa_seq RESTART WITH 1;
+DELETE FROM vals_recebidos;
+ALTER SEQUENCE vals_recebidos_id_val_seq RESTART WITH 1;
+DELETE FROM contas_receber;
+ALTER SEQUENCE contas_receber_id_conta_seq RESTART WITH 1;
+DELETE FROM sales;
+-- SELECT *FROM sales_id_sale_seq;
+ALTER SEQUENCE sales_id_sale_seq RESTART WITH 1;
+-- DELETE FROM itens_sale;
+ALTER SEQUENCE itens_sale_id_item_sequen_seq RESTART WITH 1;
+DELETE FROM refresh_token;
+ALTER SEQUENCE refresh_token_id_seq RESTART WITH 1;
+
+DELETE FROM users;
+ALTER SEQUENCE users_id_seq RESTART WITH 1;
+DELETE FROM persons;
+ALTER SEQUENCE persons_id_person_seq RESTART WITH 1;
+DELETE FROM contacts;
+ALTER SEQUENCE contacts_id_seq RESTART WITH 1;
